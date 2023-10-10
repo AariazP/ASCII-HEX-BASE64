8135676b3166ab3a363f4810f238b0a0507d6945
Alejandro Arias <josea.ariasp@uqvirtual.edu.co>
2023-10-09 20:34:50 -0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormBuilder, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hex64';
+
+
+  public form:FormGroup;
+
+  public hex:string;
+  public base64:string;
+
+  constructor(private fb: FormBuilder) {
+
+    this.form = this.fb.group({
+      text: ['']
+    });
+
+
+
+  }
+
+
+  public convert(): void {
+
+    this.hex = this.form.value.text;
+    this.base64 = this.form.value.text;
+
+    let hexValue:string = "";
+
+    for (let i = 0; i < this.hex.length; i++) {
+      hexValue +=  this.hex.charCodeAt(i).toString(16);
+    }
+
+    this.hex = hexValue;
+    this.base64 = btoa(this.form.value.text);
+
+
+  }
+
+
+
 }
